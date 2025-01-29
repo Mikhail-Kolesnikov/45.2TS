15ed1d7e182d055c4016c030c8b5fea5969e8029
mikhailkolesnikov <mikhail.kolesnikov@outlook.de>
2025-01-29 22:21:50 +0100

    new fox

--- src/lessons/fox/RandomFox.tsx
+++ src/lessons/fox/RandomFox.tsx
@@ -4,16 +4,19 @@ export default function RandomFox() {
   const [imageUrl, setImageUrl] = useState("");
 
   useEffect(() => {
-    const randomId = Math.floor(Math.random() * 123) + 1; 
-    setImageUrl(`https://randomfox.ca/images/${randomId}.jpg`);
+    fetch("https://randomfox.ca/floof/") 
+      .then(response => response.json())
+      .then(data => setImageUrl(data.image)) 
+      .catch(error => console.error("Error fetching image:", error));
   }, []); 
+
   return (
     <div>
       <h2>Random Fox 🦊</h2>
       {imageUrl ? (
         <img src={imageUrl} alt="Random Fox" width={400} height={300} style={{ borderRadius: "8px" }} />
       ) : (
-        <p>""</p>
+        <p>Loading...</p>
       )}
     </div>
   );
